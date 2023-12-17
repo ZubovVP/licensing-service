52fca74cc9fad34642b0dd24c1a1d89e1956e2e2
Vitaly Zubov <Zubov.VP@yandex.ru>
2023-12-17 23:50:27 +0300

    Add Hateoas

--- src/main/java/ru/zubov/licensingservice/controller/LicenseController.java
+++ src/main/java/ru/zubov/licensingservice/controller/LicenseController.java
@@ -8,6 +8,9 @@ import ru.zubov.licensingservice.service.LicenseService;
 
 import java.util.Locale;
 
+import static org.springframework.hateoas.server.core.DummyInvocationUtils.methodOn;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "v1/organization/{organizationId}/license")
@@ -18,6 +21,20 @@ public class LicenseController {
     public ResponseEntity<License> getLicense(@PathVariable("organizationId") String organizationId,
                                               @PathVariable("licenseId") String licenseId) {
         License license = licenseService.getLicense(licenseId, organizationId);
+
+        license.add(linkTo(methodOn(LicenseController.class)
+                        .getLicense(organizationId, license.getLicenseId()))
+                        .withSelfRel(),
+                linkTo(methodOn(LicenseController.class)
+                        .createLicense(organizationId, license, null))
+                        .withRel("createLicense"),
+                linkTo(methodOn(LicenseController.class)
+                        .updateLicense(organizationId, license))
+                        .withRel("updateLicense"),
+                linkTo(methodOn(LicenseController.class)
+                        .deleteLicense(organizationId, license.getLicenseId()))
+                        .withRel("deleteLicense"));
+
         return ResponseEntity.ok(license);
     }
 
