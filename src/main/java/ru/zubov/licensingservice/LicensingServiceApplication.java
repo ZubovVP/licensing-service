c54059518099349d79dc1bd30d021da16f8b71ea
Vitaly Zubov <Zubov.VP@yandex.ru>
2023-12-20 23:56:46 +0300

    Correct output russian text for bundle

--- src/main/java/ru/zubov/licensingservice/LicensingServiceApplication.java
+++ src/main/java/ru/zubov/licensingservice/LicensingServiceApplication.java
@@ -3,6 +3,7 @@ package ru.zubov.licensingservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -25,10 +26,10 @@ public class LicensingServiceApplication {
 
     @Bean
     public ResourceBundleMessageSource messageSource() {
-        ResourceBundleMessageSource messageSource =
-                new ResourceBundleMessageSource();
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
         messageSource.setUseCodeAsDefaultMessage(true);
         messageSource.setBasenames("messages");
+        messageSource.setDefaultEncoding("ISO-8859-1");
         return messageSource;
     }
 }
