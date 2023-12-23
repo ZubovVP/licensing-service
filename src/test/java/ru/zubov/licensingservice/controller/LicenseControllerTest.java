461c7f1e059ced9e36225fc26142499c558d5643
Vitaly Zubov <Zubov.VP@yandex.ru>
2023-12-23 23:52:43 +0300

    Correct test getLicenseByLicenseIdAndOrganizationId

--- src/test/java/ru/zubov/licensingservice/controller/LicenseControllerTest.java
+++ src/test/java/ru/zubov/licensingservice/controller/LicenseControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import ru.zubov.licensingservice.service.LicenseService;
 
 import java.util.List;
@@ -32,17 +33,17 @@ class LicenseControllerTest {
     private LicenseService licenseService;
 
     @Test
-    public void getAllEmployeesAPI() throws Exception {
-
-//        when(ingredientRepository.findAll()).thenReturn(List.of(ingredient));
-//        doCallRealMethod().when(ingredientMapper).ingredientToIngredientDto(ingredient);
-
-
+    public void getLicenseByLicenseIdAndOrganizationId() throws Exception {
         String organizationId = "organizationId";
         String licenseId = "licenseId";
         when(licenseService.getLicense(licenseId, organizationId)).thenCallRealMethod();
         mvc.perform(get("/v1/organization/{organizationId}/license/{licenseId}", organizationId, licenseId))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.licenseId",  Matchers.equalTo("licenseId")))
+                .andExpect(jsonPath("$.description",  Matchers.equalTo("Software product")))
+                .andExpect(jsonPath("$.productName",  Matchers.equalTo("Ostock")))
+                .andExpect(jsonPath("$.licenseType",  Matchers.equalTo("full")))
+                .andExpect(jsonPath("$.organizationId",  Matchers.equalTo("organizationId")));
     }
 
 
