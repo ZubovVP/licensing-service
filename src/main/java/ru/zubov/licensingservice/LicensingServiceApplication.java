c4faef32819814ec960476d87cb1cc71310512cf
Vitaly Zubov <Zubov.VP@yandex.ru>
2024-01-29 22:08:19 +0300

    Correct spring-cloud-starter

--- src/main/java/ru/zubov/licensingservice/LicensingServiceApplication.java
+++ src/main/java/ru/zubov/licensingservice/LicensingServiceApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
