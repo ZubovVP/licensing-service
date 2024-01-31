020f39e6eda99ef27ef66301a14ff8cf1fcafe47
Vitaly Zubov <Zubov.VP@yandex.ru>
2024-01-31 22:03:55 +0300

    Correct project

--- src/main/java/ru/zubov/licensingservice/config/ServiceConfig.java
+++ src/main/java/ru/zubov/licensingservice/config/ServiceConfig.java
@@ -1,10 +1,12 @@
 package ru.zubov.licensingservice.config;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.stereotype.Component;
 
 @ConfigurationPropertiesScan
 @Component
+@ConfigurationProperties(prefix = "example")
 public class ServiceConfig {
     private String property;
 
